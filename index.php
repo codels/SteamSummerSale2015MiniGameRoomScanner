57c34031b99e52ae0f3d11b6c6930d8c3d69e3dd
codels <codels25@gmail.com>
2015-06-17 23:56:08 +0300

    up

--- index.php
+++ index.php
@@ -15,7 +15,9 @@
 <?php
 date_default_timezone_set('Europe/Moscow');
 mb_internal_encoding('UTF-8');
-$result = json_decode(file_get_contents('http://steamga.me/data/api/all.json'));
+$objectsAllRooms = json_decode(file_get_contents('http://steamga.me/data/api/all.json'));
+$objectsTopRooms = json_decode(file_get_contents('http://steamga.me/data/api/leaderboard.json'));
+$roomsName = json_decode(file_get_contents('./rooms_name.json'));
 
 $endDateTime = new DateTime();
 $endDateTime->setTime(19, 00, 00);
@@ -29,28 +31,69 @@ if ($endDateTime < $currentDateTime) {
 
 $endTimeStamp = $endDateTime->getTimestamp();
 
+$maxPlayers = 1500;
+
 $array = array();
-foreach ($result as $res) {
-    $res->lvl_per_wh = $res->wormholes > 0 ? $res->level / $res->wormholes : 0;
-    $res->level_per_seconds = $res->level / (time() - $res->timestamp_game_start);
-    $res->possible_max_level = $res->level_per_seconds * ($endTimeStamp - $res->timestamp_game_start);
-    $array[] = $res;
+foreach ($objectsAllRooms as $objectRoom) {
+    $objectRoom->lvl_per_wh = $objectRoom->wormholes > 0 ? $objectRoom->level / $objectRoom->wormholes : 0;
+    $objectRoom->level_per_seconds = $objectRoom->level / (time() - $objectRoom->timestamp_game_start);
+    $objectRoom->possible_max_level = $objectRoom->level_per_seconds * ($endTimeStamp - $objectRoom->timestamp_game_start);
+    $objectRoom->name = 'unknown';
+
+    $nameFound = false;
+
+    foreach ($objectsTopRooms as $objectTopRoom) {
+        if ($objectTopRoom->id == $objectRoom->id) {
+            if ($objectTopRoom->name != 'unknown') {
+                $objectRoom->name = $objectTopRoom->name;
+                $nameFound = true;
+            }
+            break;
+        }
+    }
+
+    if (!$nameFound) {
+        foreach ($roomsName as $room) {
+            if ($room->id == $objectRoom->id) {
+                $objectRoom->name = $room->name;
+                $nameFound = true;
+                break;
+            }
+        }
+    }
+
+    $array[] = $objectRoom;
 }
 
 // render
 echo "<table  id='tablesorter-demo' class='tablesorter' border='0' cellpadding='0' cellspacing='1'>
 <thead>
 <tr>
+    <th>#</th>
     <th>Room ID</th>
+    <th>Room name</th>
+    <th>Players</th>
+    <th>Current level</th>
     <th>Level per seconds</th>
     <th>Level per wormholes</th>
     <th>Possible max level</th>
+    <th>Wormholes</th>
 </tr>
 </thead><tbody>";
-foreach ($array as $res) {
+foreach ($array as $objectRoom) {
     echo '<tr>';
 
-    echo "<td>{$res->id}</td><td>{$res->level_per_seconds}</td><td>{$res->lvl_per_wh}</td><td>{$res->possible_max_level}</td>";
+    echo "
+<td>{$objectRoom->position}</td>
+<td>{$objectRoom->id}</td>
+<td>{$objectRoom->name}</td>
+<td style='" . ($objectRoom->players == 1500 ? 'color: red;' : '') . "'>{$objectRoom->players}</td>
+<td>{$objectRoom->level}</td>
+<td>{$objectRoom->level_per_seconds}</td>
+<td>{$objectRoom->lvl_per_wh}</td>
+<td>{$objectRoom->possible_max_level}</td>
+<td>{$objectRoom->wormholes}</td>
+";
 
 //var_dump($res);
     echo '</tr>';
